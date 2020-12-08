5dbf439109cae4667243631aef7de812580842da
MaksGovor <maksgovruha@gmail.com>
2020-12-08 21:24:03 +0200

    Add test for retry

---
+++ test/retry.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fsPromises = require('fs').promises;
+const { retry, sleep } = require('./../lib/retry');
+
+async function readDelay(filename, msec) {
+  await sleep(msec);
+  return fsPromises.readFile(filename);
+}
+
+async function openDelay(filename, msec) {
+  await sleep(msec);
+  return fsPromises.open(filename, 'w');
+}
+
+openDelay('test.txt', 3000)
+  .then(() => console.log('File created'))
+  .catch(err => console.log(err.message));
+
+(async () => {
+  await retry(5, readDelay, 'test.txt', 1000);
+})();
