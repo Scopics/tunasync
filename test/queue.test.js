23ffd75940877954c542b0a941abb2370f0979d3
Anastasiya Trembach <nastia.trembach@gmail.com>
2020-12-12 22:12:58 +0200

    correct queue.pushTask

--- test/queue.test.js
+++ test/queue.test.js
@@ -2,15 +2,15 @@
 
 const queue = require('../lib/queue');
 
-const createTask = value => () => {
+const createTask = value => {
   if (value === 6) return Promise.reject(new Error('sorry'));
   return new Promise(resolve => setTimeout(() => resolve(value), value));
 };
 
 const q = new queue();
 
-q.pushTasks(createTask(1));
-q.pushTasks([createTask(2), createTask(3)]);
-q.pushTasks(createTask(6));
+q.pushTask(createTask, [1]);
+q.pushTask(createTask, [2]);
+q.pushTask(createTask, [6]);
 
 q.doTasks().then(result => console.log(result));
