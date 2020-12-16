1684e1737b210b00833044151bd108e10dcb4f82
MaksGovor <maksgovruha@gmail.com>
2020-12-16 21:12:37 +0200

    run eslint

--- test/retry.test.js
+++ test/retry.test.js
@@ -10,34 +10,34 @@ metatests.test('test retry succes', test => {
 
   const asyncSum = async (a, b) => {
     await sleep(100);
-    if ( a + b < minSum) {
+    if (a + b < minSum) {
       minSum -= a + b;
       throw new Error('Sorry');
     }
     return a + b;
-  }
+  };
 
   retry(asyncSum, [2, 3], { retries: 5, interval: 10 })
-    .then(data => test.strictSame(data, expectedResult))
-    
+    .then(data => test.strictSame(data, expectedResult));
+
   test.end();
 });
 
-metatests.test('test retry error' , test => {
+metatests.test('test retry error', test => {
   let minSum = 100;
   const expectedResult = new Error('Sorry');
 
   const asyncSum = async (a, b) => {
     await sleep(100);
-    if ( a + b < minSum) {
+    if (a + b < minSum) {
       minSum -= a + b;
       throw new Error('Sorry');
     }
     return a + b;
-  }
+  };
 
   retry(asyncSum, [2, 3], { retries: 5, interval: 10 })
     .catch(err => test.strictSame(err, expectedResult));
-  
+
   test.end();
 });
