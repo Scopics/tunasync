56a09e665cdbe8aff77cd8ea73308997258e6e12
Anastasiya Trembach <nastia.trembach@gmail.com>
2020-12-22 16:41:09 +0200

    add test series with err

--- test/series.test.js
+++ test/series.test.js
@@ -3,9 +3,11 @@
 const metatests = require('metatests');
 const { series } = require('./../lib/series');
 
-const createFn = val => (err, cb) => {
-  if (val === 6) return Promise.reject(new Error('sorry'));
-  return new Promise(resolve => setTimeout(() => resolve(cb(err, val)), val));
+const createFn = value => (err, cb) => {
+  setTimeout(() => {
+    if (value === 6) return cb(new Error('sorry'));
+    else return cb(err, value);
+  }, value);
 };
 
 metatests.test('test series', test => {
@@ -23,4 +25,18 @@ metatests.test('test series', test => {
   test.end();
 });
 
+metatests.test('test series with error', test => {
+  const fns = [createFn(2000), createFn(6), createFn(500)];
+  const expectedResult = {
+    err: new Error('sorry'),
+    data: undefined
+  };
+
+  series(fns, (err, data) => {
+    const result = { err, data };
+    test.strictSame(result, expectedResult);
+  });
+
+  test.end();
+});
 
