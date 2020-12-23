966709cd36982566e79e745976ad2678bd1a4788
MaksGovor <maksgovruha@gmail.com>
2020-12-23 18:32:02 +0200

    Add metatests for map (#3)

--- test/map.test.js
+++ test/map.test.js
@@ -1,17 +1,63 @@
 'use strict';
 
+const metatests = require('metatests');
 const map = require('../lib/map');
-const arr = [1, 2, 4, 5, 2, '12'];
+const { sleep } = require('../lib/utils/promisify');
+const arr = [1, 2, 3, 5, 2, '12'];
 
+metatests.test('test async callback function', test => {
+  const expectedResult = [ 3, 4, 6, 7, 4, '122' ];
+  map((item, cb) => {
+    setTimeout(() => {
+      cb(null, item + 2);
+    }, 10);
+  }, arr, { isCb: true })
+    .then(data => test.strictSame(data, expectedResult));
 
-const sleep = msec => new Promise(resolve => {
-  setTimeout(resolve, msec);
+  Error;
+  const expectedError = new Error('Sorry');
+  map((item, cb) => {
+    setTimeout(() => {
+      if (item === 2) cb(new Error('Sorry'), null);
+      cb(null, item + 2);
+    }, 10);
+  }, arr, { isCb: true })
+    .catch(err => test.strictSame(err, expectedError));
+
+  test.end();
 });
 
-(async () => {
-  map((item, cb) => {
-    cb(null, item + 2);
-  }, arr, { parallel: false, isCb: true})
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
-})();
+metatests.test('test async promise function', test => {
+  const expectedResult = [ 3, 4, 6, 7, 4, '122' ];
+  map(async item => {
+    await sleep(10);
+    return item + 2;
+  }, arr)
+    .then(data => test.strictSame(data, expectedResult));
+
+  // Parallel
+  map(async item => {
+    await sleep(10);
+    return item + 2;
+  }, arr, { parallel: true })
+    .then(data => test.strictSame(data, expectedResult));
+
+  // Error
+  const expectedError = new Error('Sorry');
+  map(async item => {
+    await sleep(10);
+    if (item === 2) throw new Error('Sorry');
+    return item + 2;
+  }, arr)
+    .catch(err => test.strictSame(err, expectedError));
+
+  // Parallel error
+  map(async item => {
+    await sleep(10);
+    if (item === 2) throw new Error('Sorry');
+    return item + 2;
+  }, arr, { parallel: true })
+    .catch(err => test.strictSame(err, expectedError));
+
+  test.end();
+});
