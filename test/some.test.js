4eddcd59d4595e70809861853c0c2a0ad60edf46
tedi4t <tedi4t@gmail.com>
2020-12-30 19:13:07 +0200

    adding test for some

--- test/some.test.js
+++ test/some.test.js
@@ -1,12 +1,33 @@
 'use strict';
 
 const fs = require('fs');
+const metatests = require('metatests');
 const some = require('./../lib/some');
+const { sleep } = require('../lib/utils/promisify');
 
-some((filePath, callback) => {
-  fs.access(filePath, err => {
-    callback(null, !err);
-  });
-}, ['file1', 'file2', 'retry.test.js'])
-  .then(res => console.log(res))
-  .catch(err => console.log(err.message));
+
+metatests.test('test some 1', test => {
+  const expectedResult = true;
+  some((filePath, callback) => {
+    fs.access(filePath, err => {
+      callback(null, !err);
+    });
+  }, ['file1', 'file2', 'retry.test.js'],
+  { isCb: true })
+    .then(res => test.strictSame(res, expectedResult))
+    .catch(err => console.log(err.message));
+
+  test.end();
+});
+
+metatests.test('test some 2', test => {
+  const expectedResult = true;
+  some(async str => {
+    await sleep(1000);
+    return str.length === 5;
+  }, ['text11', 'text1', 'text'])
+    .then(res => test.strictSame(res, expectedResult))
+    .catch(err => console.log(err.message));
+
+  test.end();
+});
