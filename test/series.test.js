80f5c1a72feee51ef83ef08eea1f7d09e357bf72
Anastasiya Trembach <nastia.trembach@gmail.com>
2020-12-30 16:51:21 +0200

    add more tests

--- test/series.test.js
+++ test/series.test.js
@@ -10,6 +10,11 @@ const createFn = value => (err, cb) => {
   }, value);
 };
 
+const createAsyncFn = value => () => {
+  if (value === 6) return Promise.reject((new Error('sorry')));
+  return new Promise(resolve => setTimeout(() => resolve((value)), value));
+};
+
 metatests.test('test series', test => {
   const fns = [createFn(2000), createFn(60), createFn(500)];
   const expectedResult = {
@@ -40,3 +45,32 @@ metatests.test('test series with error', test => {
   test.end();
 });
 
+metatests.test('test series async', test => {
+  const fns = [createAsyncFn(2000), createAsyncFn(60), createAsyncFn(500)];
+  const expectedResult = {
+    err: null,
+    data: [2000, 60, 500]
+  };
+
+  series(fns, (err, data) => {
+    const result = { err, data };
+    test.strictSame(result, expectedResult);
+  }, 1);
+
+  test.end();
+});
+
+metatests.test('test series async with error', test => {
+  const fns = [createAsyncFn(2000), createAsyncFn(6), createAsyncFn(500)];
+  const expectedResult = {
+    err: new Error('sorry'),
+    data: undefined
+  };
+
+  series(fns, (err, data) => {
+    const result = { err, data };
+    test.strictSame(result, expectedResult);
+  }, 1);
+
+  test.end();
+});
