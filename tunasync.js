c904f6554ba16b11751b931558d457abda290c27
MaksGovor <maksgovruha@gmail.com>
2021-01-18 20:23:21 +0200

    Add comments

--- tunasync.js
+++ tunasync.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const submodules = [
-  'asyncEmitter',
-  'map',
-  'memoize',
-  'queue',
-  'reduce',
-  'retry',
-  'series',
-  'some',
+  'asyncEmitter', // asynchronous Event emitter
+  'map', // async map array method
+  'memoize', // memorization of asynchronous functions
+  'queue', // asynchronous queue
+  'reduce', // async reduce array method
+  'retry', // repeating the function call until the result
+  'series', // calling functions depends on the result of the previous
+  'some', // async some array method
 ].reduce((obj, path) => (
   obj[path] = require('./lib/' + path), obj), {});
 
